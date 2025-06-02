f687005b754900c64b8cad6a4e9dc0d6f202ef2b
jennajhki <jnki4090@gmail.com>
2025-06-02 17:17:56 +0900

    edited 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 
 import { Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
+// import Sidebar from './components/Sidebar';
 
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Journal from './pages/Journal';
-import Discussion from './pages/Discussion';
+// import Discussion from './pages/Discussion';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="layout">
-      <Sidebar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/journal" element={<Journal />} />
-          <Route path="/discussion" element={<Discussion />} />
-        </Routes>
-      </main>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/journal" element={<Journal />} />
+    </Routes>
   );
 };
 
